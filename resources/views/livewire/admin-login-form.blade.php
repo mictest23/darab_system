7c0d624f00ec5083e97f6feb0450048ea3574a3d
mictest23 <mictestone@gmail.com>
2023-05-09 12:38:42 +0800

    commit from main some error

---
+++ resources/views/livewire/admin-login-form.blade.php
@@ -0,0 +1,56 @@
+<div>
+    
+
+    @if (Session::get('fail'))
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <span class="block sm:inline">{{ Session::get('fail') }}</span>
+    </div>
+    @endif
+
+    @if (Session::get('success'))
+        <div class="alert alert-success">
+            {!! Session::get('success') !!}
+        </div>
+    @endif
+
+    <form class="mt-4" method="post" wire:submit.prevent="LoginHandler()" autocomplete="off" novalidate="">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            wire:model="username"
+          />
+          @error('username')
+              <p class="text-red-500 text-sm">{{ $message }}</p>
+          @enderror
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="**********"
+            wire:model="password"
+          />
+          @error('password')
+              <p class="text-red-500 text-sm">{{ $message }}</p>
+          @enderror
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+
+</div>
